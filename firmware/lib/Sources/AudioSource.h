56a690300e46d50f04c8d7f11885c75d915c4503
Jean-Michel Cazaux <jeanmichel.cazaux@gmail.com>
2026-02-22 16:32:00 +0100

    Basic implementation

--- firmware/lib/Sources/AudioSource.h
+++ firmware/lib/Sources/AudioSource.h
@@ -1,12 +1,17 @@
 
 
 class AudioSource {
-  public:
-   const char* name;
-   const bool needsRadio;
-   const bool needsBluetooth;
-   const bool needsLowCpuFrequency;
+   public:
+    const char* name;
+    const bool needsRadio = false;
+    const bool needsBluetooth = false;
+    const bool needsLowCpuFrequency = false;
 
-   virtual void tuneUp(bool fast);
-   virtual void tuneDown(bool fast);
+    virtual void tuneUp(){};
+    virtual void tuneDown(){};
+    virtual void tunePressed(){};
+    virtual void tuneLongPressed(){};
+    virtual void tuneDoublePressed(){};
+
+    AudioSource(const char* _name) : name(_name) {};
 };
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,19 +1,46 @@
 #include <AdvancedLogger.h>
 #include <Arduino.h>
+#include <AudioSource.h>
 #include <Display.h>
 #include <LCDDisplay.h>
 #include <LittleFS.h>
+#include <RotaryEncoder.h>
 
 #include <string>
 
 #define ST(A) #A
 #define STR(A) ST(A)
 
+// Pins
+#define SELECTOR_ENCODER_SW 5    // to SW pin of the mode selector rotary encoder
+#define SELECTOR_ENCODER_DT 16   // to DT pin of the mode selector rotary encoder
+#define SELECTOR_ENCODER_CLK 17  // to CLK pin of the mode selector rotary encoder
+
 // Logging related
 const char* LOG_FILE_PATH = "/internal/log.txt";
 const ulong MAX_LOG_LINES = 500;
 
+// Delays & timings
+const ulong SWITCH_SOURCE_DELAY = 400;  // Delay between a source is selected and the source become active (avoid switching source at each encoder tick)
+
 Display* display;
+
+static const char RADIO_FM[] = "Radio FM";
+static const char RADIO_DAB[] = "Radio DAB";
+static const char BLUETOOTH[] = "Bluetooth";
+
+AudioSource fm = AudioSource(RADIO_FM);
+AudioSource dab = AudioSource(RADIO_DAB);
+AudioSource bluetooth = AudioSource(BLUETOOTH);
+
+AudioSource sources[] = {fm, dab, bluetooth};
+uint8_t nbSources = std::size(sources);
+
+uint8_t currentSource = 0;
+int selectedSource = -1;
+long selectedSourceTime = millis();
+
+RotaryEncoder selectorEncoder(SELECTOR_ENCODER_DT, SELECTOR_ENCODER_CLK, RotaryEncoder::LatchMode::TWO03);
 
 void setup() {
     Serial.begin(MONITOR_SPEED);
@@ -30,6 +57,11 @@ void setup() {
 
     LOG_INFO("Initializing systems...");
     LOG_INFO("*** Version %s ***", STR(VERSION));
+
+    LOG_INFO("Initializing Audio sources: ");
+    for (u_int8_t i = 0; i < nbSources; i++) {
+        LOG_INFO(" -> #%d: %s", i + 1, sources[i].name);
+    }
 
     char versionString[30];
     sprintf(versionString, "* Version %s *", STR(VERSION));
@@ -49,10 +81,39 @@ void setup() {
     display->displayLine("La plus grande matinale de France avec Florence Paracuellos", 3, ROLLING_LEFT);
 
     LOG_INFO("Systems initialized");
+    display->displayLine(sources[currentSource].name, 0);
 }
 
 void loop() {
-    LOG_DEBUG("Starting main loop");
+    static int selectorPosition = 0;
+
+    // Tick everything that needs ticking
     display->tick(millis());
+    selectorEncoder.tick();
+
+
+    int newPos = selectorEncoder.getPosition();
+    if (selectorPosition != newPos) {
+        selectedSource = (newPos / 2) % nbSources;
+        // When turning anti-clockwise, we want to go from the first to the last, then one before the last etc.
+        selectedSource = (selectedSource < 0 ? selectedSource + nbSources : selectedSource);
+        selectedSourceTime = millis();
+        selectorPosition = newPos;
+
+        LOG_INFO("Selecting source #%d (pos %d)", selectedSource, selectorPosition);
+
+        display->displayLine(sources[selectedSource].name, 0);
+    }
+
+    if (selectedSource >= 0 && (selectedSourceTime + SWITCH_SOURCE_DELAY) < millis()) {
+        currentSource = selectedSource;
+        selectedSource = -1;
+        selectedSourceTime = 0;
+
+        display->displayLine(sources[currentSource].name, 0);
+        LOG_INFO("Switched to source %d", currentSource);
+    }
+
+
     delay(10);
 }
