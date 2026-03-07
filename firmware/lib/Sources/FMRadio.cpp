3cdecf4c31ed6e4b1b7c995e79ef72158b18fbd3
Jean-Michel Cazaux <jeanmichel.cazaux@gmail.com>
2026-03-07 09:03:05 +0100

    Refactor a bit FMRadio

--- firmware/lib/Sources/FMRadio.cpp
+++ firmware/lib/Sources/FMRadio.cpp
@@ -594,3 +594,46 @@ void FMRadio::savePresets() {
     file.close();
 }
 
+bool FMRadio::ServiceInfo::operator==(const FMRadio::ServiceInfo &other) const {
+    if (this->frequency != other.frequency) return false;
+
+    if (this->year != other.year) return false;
+    if (this->month != other.month) return false;
+    if (this->day != other.day) return false;
+    if (this->hour != other.hour) return false;
+    if (this->minute != other.minute) return false;
+
+    if (this->signalStrength != other.signalStrength) return false;
+    if (this->snr != other.snr) return false;
+
+    if (strcmp(this->serviceName, other.serviceName) != 0) return false;
+    if (strcmp(this->serviceData, other.serviceData) != 0) return false;
+
+    return true;
+}
+
+void FMRadio::ServiceInfo::copyFrom(const ServiceInfo &other) {
+    this->frequency = other.frequency;
+
+    this->year = other.year;
+    this->month = other.month;
+    this->day = other.day;
+    this->hour = other.hour;
+    this->minute = other.minute;
+
+    this->signalStrength = other.signalStrength;
+    this->snr = other.snr;
+
+    strcpy(this->serviceName, other.serviceName);
+    strcpy(this->serviceData, other.serviceData);
+}
+
+void FMRadio::ServiceInfo::clear() {
+    this->frequency = 0;
+    this->signalStrength = 0;
+    this->snr = 0;
+    strcpy(this->serviceName, "");
+    strcpy(this->serviceData, "");
+}
+
+
--- firmware/lib/Sources/FMRadio.h
+++ firmware/lib/Sources/FMRadio.h
@@ -41,13 +41,47 @@ public:
     void modeDoublePressed() override;
 
 private:
+    class ServiceInfo {
+    public:
+        uint16_t frequency = 0;
+
+        char serviceName[64] = "";
+        char serviceData[128] = "";
+
+        uint16_t year = 0;
+        uint8_t month = 0;
+        uint8_t day = 0;
+        uint8_t hour = 0;
+        uint8_t minute = 0;
+
+        int8_t signalStrength = 0;
+        int8_t snr = 0;
+
+        bool operator==(const ServiceInfo &other) const;
+
+        void copyFrom(const ServiceInfo &other);
+
+        void clear();
+    };
+
+    struct Preset {
+        uint16_t frequency = 8750;
+        char name[32] = "";
+    };
+
     DAB *dab;
     Preferences preferences;
-    unsigned long lastTargetFrequencyChange = 0;
-    uint16_t targetFrequency = 0;
+    ServiceInfo serviceInfo{};
+
+    std::vector<Preset> listPresets;
+    std::vector<Preset> memoryPresets;
     uint8_t currentMode = 0;
     uint8_t currentPresetIndex = 0;
     uint8_t currentMemoryIndex = 0;
+
+    unsigned long lastTargetFrequencyChange = 0;
+    uint16_t targetFrequency = 0;
+
     bool memorizingPreset = false; // True when we are in the process of memorizing a preset
     uint8_t targetMemoryPreset = 0; // Memory preset where
 
@@ -60,6 +94,8 @@ private:
     void tuneList(TuneDirection direction);
 
     void tuneMemory(TuneDirection direction);
+
+    void tunePreset(const Preset &preset);
 
     void selectTargetMemoryPreset(TuneDirection direction);
 
@@ -79,82 +115,11 @@ private:
 
     void savePresets();
 
-    char *getServiceNameFromPresets(uint16_t frequency);
-
-    struct ServiceInfo {
-        uint16_t frequency;
-
-        char serviceName[64];
-        char serviceData[128];
-
-        uint16_t year;
-        uint8_t month;
-        uint8_t day;
-        uint8_t hour;
-        uint8_t minute;
-
-        int8_t signalStrength;
-        int8_t snr;
-
-        // TODO: the below would rather be in the cpp file, but failed to do this
-        bool operator==(const ServiceInfo &other) const {
-            if (this->frequency != other.frequency) return false;
-
-            if (this->year != other.year) return false;
-            if (this->month != other.month) return false;
-            if (this->day != other.day) return false;
-            if (this->hour != other.hour) return false;
-            if (this->minute != other.minute) return false;
-
-            if (this->signalStrength != other.signalStrength) return false;
-            if (this->snr != other.snr) return false;
-
-            if (strcmp(this->serviceName, other.serviceName) != 0) return false;
-            if (strcmp(this->serviceData, other.serviceData) != 0) return false;
-
-            return true;
-        };
-
-        void copyFrom(const ServiceInfo &other) {
-            this->frequency = other.frequency;
-
-            this->year = other.year;
-            this->month = other.month;
-            this->day = other.day;
-            this->hour = other.hour;
-            this->minute = other.minute;
-
-            this->signalStrength = other.signalStrength;
-            this->snr = other.snr;
-
-            strcpy(this->serviceName, other.serviceName);
-            strcpy(this->serviceData, other.serviceData);
-        };
-
-        void clear() {
-            this->frequency = 0;
-            this->signalStrength = 0;
-            this->snr = 0;
-            strcpy(this->serviceName, "");
-            strcpy(this->serviceData, "");
-        };
-    };
-
-    ServiceInfo serviceInfo{};
-
-    struct Preset {
-        uint16_t frequency = 8750;
-        char name[32] = "";
-    };
-
-    std::vector<Preset> listPresets;
-    std::vector<Preset> memoryPresets;
-
-    void updatePresetsServiceName(const ServiceInfo &serviceInfo);
-
     String presetsAsJson();
 
     void loadPresetsFromJson(String jsonString);
 
-    void tunePreset(const Preset &preset);
+    char *getServiceNameFromPresets(uint16_t frequency);
+
+    void updatePresetsServiceName(const ServiceInfo &serviceInfo);
 };
